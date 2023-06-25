6e645076da3321ce56b6c5d86c77c4c54692f831
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-06-25 14:15:11 +0100

    Add files via upload

--- builder.sh
+++ builder.sh
@@ -1,7 +1,4 @@
-mcs -unsafe kernel.cs -o kernel.exe
-mono --aot=asmonly kernel.exe
-cp format.asm out.s
-cat kernel.exe.s >> out.s
-as -o hello.o out.s
-ld link.ld hello.o -o kernel.elf
-
+printf "\ec\e[44;37m\n"
+mcs Program.cs -o $1
+mono --aot=asmonly  $1
+cat $1.s
