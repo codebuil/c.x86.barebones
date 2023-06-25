8765e506a99815389a2774ff9d0871d06b06301e
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-06-26 00:28:29 +0100

    Add files via upload

--- builder.sh
+++ builder.sh
@@ -1,8 +1,4 @@
 printf "\ec\e[44;37m\n"
 mcs $1 -o $1.exe
-mono --aot=asmonly  $1.exe
-cp format.asm out.s
-cat $1.exe.s >> out.s
-as -o out.o out.s
-ld link.ld out.o -o out.elf
+mkbundle -o out.a $1.exe
 
