eb39c000381d3248aa48c01295c9349a1ebb476f
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-06-26 16:39:08 +0100

    Add files via upload

--- builder.sh
+++ builder.sh
@@ -1,4 +1,11 @@
 printf "\ec\e[44;37m\n"
-mcs $1 -o $1.exe
-mkbundle -o out.a $1.exe
-
+rm *.o
+rm *.elf
+rm *.img
+rm *.exe
+rm *.dll
+nasm -felf32 -o boot.o boot.S
+gcc -c kernel.c -o kernel.o -nostdlib
+gcc link.ld boot.o kernel.o -o kernel.elf -nostdlib
+objcopy kernel.elf -O binary kernel.bin
+qemu-system-i386 -kernel kernel.elf
